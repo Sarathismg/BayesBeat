c0dff3e7c17d6b02d1d9c090a548027408035643
snigdha-datalab <sarathismg@gmail.com>
2020-10-21 02:03:05 +0600

    remove duplicates

--- bayesbeat/bayesian/__init__.py
+++
@@ -1,4 +0,0 @@
-from .Bayes_modules.BayesLinear import BayesLinear as BBB_LRT_Linear
-from .Bayes_modules.BayesConv import BayesConv1d as BBB_LRT_Conv1d
-
-from .misc import FlattenLayer, ModuleWrapper
--- bayesbeat/bayesian/utils.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-def logmeanexp(x, dim=None, keepdim=False):
-    if dim is None:
-        x, dim = x.view(-1), 0
-    x_max, _ = torch.max(x, dim, keepdim=True)
-    x = x_max + torch.log(torch.mean(torch.exp(x - x_max), dim, keepdim=True))
-    return x if keepdim else x.squeeze(dim)
